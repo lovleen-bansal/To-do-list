9bb612ea741f91d42aeb0fa70b088a29a12c507e
lovleen-bansal <lovleenbansal2021@gmail.com>
2024-07-08 15:22:17 +0530

    add files

---
+++ ToDoList.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoList {
+    private List<Task> tasks;
+
+    public ToDoList() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(String description) {
+        Task task = new Task(description);
+        tasks.add(task);
+    }
+
+    public void removeTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.remove(index);
+        } else {
+            System.out.println("Invalid index");
+        }
+    }
+
+    public void markTaskAsCompleted(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.get(index).markAsCompleted();
+        } else {
+            System.out.println("Invalid index");
+        }
+    }
+
+    public void displayTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks in the list.");
+        } else {
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + ". " + tasks.get(i));
+            }
+        }
+    }
+}
